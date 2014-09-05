22b033afa633ad007782869f68ba9b0c94f7179e
victor.zou <victor.zou@ef.com>
2014-09-05 16:16:10 +0800

    adding login page

---
+++ js/app.js
@@ -0,0 +1,14 @@
+﻿
+
+var greenFieldApp = angular.module('greenFieldApp', [
+    'ngRoute'
+]);
+
+greenFieldApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when("/login", {
+        controller: "",
+        templateUrl: ""
+    }).otherwise({
+        redirectTo: "/home"
+    });
+}]);
