1c4ac7da78c6cbaf57847870664868ee6e7fac65
victor.zou <victor.zou@ef.com>
2014-10-07 10:38:17 +0800

    adding books page

---
+++ app/controllers/booksController.js
@@ -0,0 +1,10 @@
+﻿
+app.controller('booksController', ['$scope', 'booksService', function ($scope, booksService) {
+    $scope.books = [];
+
+    booksService.getBooks().then(function (results) {
+        $scope.books = results.data;
+    }, function (error) {
+        console.log(error.data.message);
+    });
+}]);
---
+++ app/services/booksService.js
@@ -0,0 +1,15 @@
+﻿
+app.factory('booksService', ['$http', function ($http) {
+    var serviceBase = "http://localhost:59584/";
+    var booksServiceFactory = {};
+
+    var _getBooks = function () {
+        return $http.get(serviceBase + 'api/books').then(function (results) {
+            return results;
+        });
+    };
+    
+    booksServiceFactory.getBooks = _getBooks;
+
+    return booksServiceFactory;
+}]);
