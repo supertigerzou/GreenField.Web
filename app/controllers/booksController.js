4e4c913cae15406e7a8f4f846aa568decf097684
victor.zou <victor.zou@ef.com>
2014-10-11 17:41:15 +0800

    adding picture support

--- app/controllers/booksController.js
+++ app/controllers/booksController.js
@@ -1,6 +1,7 @@
 ﻿
 app.controller('booksController', ['$scope', 'booksService', function ($scope, booksService) {
     $scope.books = [];
+    $scope.imageBase = "http://localhost:59584/";
 
     booksService.getBooks().then(function (results) {
         $scope.books = results.data;
